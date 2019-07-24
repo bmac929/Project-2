3ffa29915b3aaeb86de953c520f7ef6c3fdb8409
NideverJoe <nidever.joe@gmail.com>
2019-07-24 11:56:34 -0500

    working character and speech automatic on page load

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+  app.get("/hub", function(req, res) {
+    db.UserData.findAll({}).then(function(dbExamples) {
+      res.render("hub", {
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -30,13 +30,13 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
