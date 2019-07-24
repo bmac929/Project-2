36c2fd9d98749964d76f03dd7bb39660ad5e56ae
NideverJoe <nidever.joe@gmail.com>
2019-07-24 10:07:42 -0500

    adding images, grid test, and character test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,24 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/rps", function(req, res) {
+    db.UserData.findAll({}).then(function(dbExamples) {
+      res.render("rps", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  app.get("/collector", function(req, res) {
+    db.UserData.findAll({}).then(function(dbExamples) {
+      res.render("collector", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
